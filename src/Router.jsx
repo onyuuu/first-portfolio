ed3b096f5428b4be8ba838ce202d45bedc0f8c0b
u y <yu@onyu.local>
2023-03-22 11:48:07 +0900

    about me 사진 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Kurly from './routes/Kurly';
 import Job from './routes/Job';
