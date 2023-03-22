40cd35b014dea7846a4b083547f272b9ad31cfcb
u y <yu@onyu.local>
2023-03-22 11:41:03 +0900

    브라우저라우터 다시 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Kurly from './routes/Kurly';
 import Job from './routes/Job';
@@ -7,12 +7,13 @@ import Job from './routes/Job';
 
 export default function Router() {
     return (
-        
+        <HashRouter>
             <AnimatePresence>
                 <Routes>
                     <Route path='/Kurly' element={<Kurly />} />
                     <Route path='/Job' element={<Job />} />
                 </Routes>
             </AnimatePresence>
+        </HashRouter>
     );
 }
