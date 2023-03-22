3ca6805c4320e40eff13c3da8cff17f1ebf35b9b
u y <yu@onyu.local>
2023-03-22 11:30:15 +0900

    브라우저라우터 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Kurly from './routes/Kurly';
 import Job from './routes/Job';
@@ -7,13 +7,12 @@ import Job from './routes/Job';
 
 export default function Router() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        
             <AnimatePresence>
                 <Routes>
                     <Route path='/Kurly' element={<Kurly />} />
                     <Route path='/Job' element={<Job />} />
                 </Routes>
             </AnimatePresence>
-        </BrowserRouter>
     );
 }
